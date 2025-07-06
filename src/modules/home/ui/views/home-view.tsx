cb823f5e5c2f8824d376ce606663843cc66af8c9
Ali Gomaa <3looosh56565656@gmail.com>
2025-07-06 21:53:41 +0300

    08:tRPC Setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,28 +1,15 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-
-import { authClient } from '@/lib/auth-client';
-import { useRouter } from 'next/navigation';
+import { useTRPC } from '@/trpc/client';
+import { useQuery } from '@tanstack/react-query';
 
 export function HomeView() {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-
-  if (!session) {
-    return <div>Loading...</div>;
-  }
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: 'Ali' }));
 
   return (
     <div className="p-4 flex flex-col gap-4">
-      <h1>Welcome {session.user?.name}</h1>
-      <Button onClick={() => authClient.signOut({
-        fetchOptions: {
-          onSuccess: () => {
-            router.push('/sign-in');
-          },
-        },
-      })}>Sign out</Button>
+      {data?.greeting}
     </div>
   );
 }
